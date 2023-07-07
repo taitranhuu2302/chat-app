f4b796bd872fde8ecaa5d78981705ed19cd36254
Tai Tran <tai.tran@mgisolutions.com>
2023-07-07 15:55:47 +0700

    feat(app): group

--- client/components/Modals/ModalCreateGroup.tsx
+++ client/components/Modals/ModalCreateGroup.tsx
@@ -8,6 +8,11 @@ import useTranslate from '@/hooks/useTranslate';
 import { useGetFriendByUser } from '@/service/UserService';
 import { AuthContext, AuthContextType } from 'contexts/AuthContext';
 import { flatMapObjectInfinite } from '@/utils/ArrayUtils';
+import { toast } from 'react-hot-toast';
+import { useCreateConversation } from '@/service/ConversationService';
+import { getErrorResponse } from '@/utils/ErrorUtils';
+import { useQueryClient } from 'react-query';
+import { API } from '@/constants/Api';
 
 interface IModalCreateGroup {
 
@@ -57,15 +62,48 @@ const ModalCreateGroup: React.FC<IModalCreateGroup> = () => {
   const t = useTranslate();
   const { auth } = useContext(AuthContext) as AuthContextType;
   const [friends, setFriends] = useState<UserType[]>([])
-  const { } = useGetFriendByUser({
+  const [members, setMembers] = useState<string[]>([])
+  const [groupName, setGroupName] = useState<string>("")
+  useGetFriendByUser({
     options: {
       onSuccess: (data: any) => {
-        setFriends(flatMapObjectInfinite(data))
-        console.log(friends);
+        setFriends(flatMapObjectInfinite(data).map((item: any) => item.friend))
       }
     },
     id: auth?._id
   })
+  const { mutateAsync: createConversation, isLoading: createLoading } = useCreateConversation({ options: {} })
+  const queryClient = useQueryClient()
+
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault()
+    console.log(!groupName || !!members.length);
+    if (!groupName || !members.length) {
+      toast.error("Please enter all fields")
+      return;
+    }
+
+    const payload: ConversationCreate = {
+      conversationType: "GROUP",
+      members,
+      conversationName: groupName
+    }
+
+    try {
+      await createConversation(payload)
+      await queryClient.refetchQueries([API.CONVERSATION.FIND_ALL_BY_USER])
+      toast.success("Create group successfully")
+      setGroupName("")
+      setMembers([])
+    } catch (e: any) {
+      const errors = getErrorResponse(e.message)
+      toast.error(errors[0])
+    }
+  }
+
+  const onChangeSelect = (selectOptions: any) => {
+    setMembers([...selectOptions.map((item: any) => item.value)])
+  }
 
   return <>
     <div>
@@ -82,10 +120,10 @@ const ModalCreateGroup: React.FC<IModalCreateGroup> = () => {
             </label>
           </div>
           <Divider />
-          <form className={'flex flex-col gap-2.5 h-full'}>
+          <form className={'flex flex-col gap-2.5 h-full'} onSubmit={handleSubmit}>
             <div className={'flex flex-col gap-2.5'}>
               <label htmlFor='group-name' className={'text-md'}>{t.home.tab.group.createGroup.groupName.label}</label>
-              <input type='text' id={'group-name'} placeholder={t.home.tab.group.createGroup.groupName.hint} className={'outline-none border dark:bg-via-300 py-1.5 px-2.5 rounded'} />
+              <input value={groupName} onChange={(e) => setGroupName(e.target.value)} type='text' id={'group-name'} placeholder={t.home.tab.group.createGroup.groupName.hint} className={'outline-none border dark:bg-via-300 py-1.5 px-2.5 rounded'} />
             </div>
             <div className={'flex flex-col gap-2.5'}>
               <label htmlFor='group-members' className={'text-md'}>{t.home.tab.group.createGroup.groupMember.label}</label>
@@ -96,6 +134,7 @@ const ModalCreateGroup: React.FC<IModalCreateGroup> = () => {
                 className={'select-custom'}
                 placeholder={t.home.tab.group.createGroup.groupMember.hint}
                 classNamePrefix="select"
+                onChange={onChangeSelect}
                 styles={theme === 'dark' ? colorStyles : {}}
               />
               <div className={'flex flex-col gap-2.5'}>
@@ -106,7 +145,15 @@ const ModalCreateGroup: React.FC<IModalCreateGroup> = () => {
 
             <div className='modal-action flex-grow'>
               <label htmlFor='modal-create-group' className='btn btn-sm btn-error'>{t.home.tab.group.createGroup.closeText}</label>
-              <label htmlFor='modal-create-group' className='btn btn-sm btn-primary'>{t.home.tab.group.createGroup.submitText}</label>
+              <button disabled={createLoading} className='btn btn-sm btn-primary'>
+                {
+                  createLoading ? (
+                    <span className="loading loading-spinner loading-sm"></span>
+                  ) : (
+                    <span>{t.home.tab.group.createGroup.submitText}</span>
+                  )
+                }
+              </button>
             </div>
           </form>
         </div>
